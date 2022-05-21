ed11d76f494ffd43afea16f411e3f874f1ddec6c
Jimbei <contrerasr2272@gmail.com>
2022-05-21 13:20:43 -0400

    null name fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,8 +5,20 @@ var randomNumber = function(min, max){
     return value;
 }
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+      }
+  
+    console.log("Your robot's name is " + name);
+    return name;
+  };
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -207,6 +219,7 @@ for(var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
         //Alert players that they are starting the round
        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+       
    
        //pick new enemy to fight based on the index of the enemy.names array
     var pickedEnemyObj = enemyInfo[i];
