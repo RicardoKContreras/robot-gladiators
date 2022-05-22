9695074818dc4f5e4abd4aea9d3c0539bac54ba1
Jimbei <contrerasr2272@gmail.com>
2022-05-22 18:01:51 -0400

    fix save and load high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -91,7 +91,9 @@ var enemyInfo = [
         return true;
       }
     }
+    if (promptFight === "fight"){
     return false;
+    }
   }
 
 
@@ -173,6 +175,26 @@ var startGame = function(){
         else {
             window.alert("You've lost your robot in battle.");
         }
+
+        window.alert("The game has now ended. Let's see how you did!");
+
+        // check localStorage for high score, if it's not there, use 0
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+          highScore = 0;
+        }
+        // if player has more money than the high score, player has new high score!
+        if (playerInfo.money > highScore) {
+          localStorage.setItem("highscore", playerInfo.money);
+          localStorage.setItem("name", playerInfo.name);
+      
+          alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        } 
+        else {
+          alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
+      
+
     //ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
     
